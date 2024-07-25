083e53a23e253120c65eca945e7a12dbc9c46ad7
tiagoforward <devtiagoforward@gmail.com>
2024-07-25 12:28:06 -0300

    added new paging files

---
+++ src/pages/Blog/index.tsx
@@ -0,0 +1,9 @@
+import { BlogContainer } from "./styles";
+
+export function Blog() {
+    return (
+        <BlogContainer>
+            <h1>Seção blog</h1>
+        </BlogContainer>
+    )
+}
---
+++ src/pages/Post/index.tsx
@@ -0,0 +1,9 @@
+import { PostContainer } from "./styles";
+
+export function Post() {
+    return (
+        <PostContainer>
+            <h1>Seção Post</h1>
+        </PostContainer>
+    )
+}
