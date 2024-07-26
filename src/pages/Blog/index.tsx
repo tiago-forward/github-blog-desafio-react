a0ae3fa133f13d3ca570937ba205d5ac3e935d9b
tiagoforward <devtiagoforward@gmail.com>
2024-07-26 20:49:10 -0300

    added user data via github api

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -3,8 +3,25 @@ import { BlogContainer, PostCardContainer } from "./styles";
 import { ProfileCard } from "../../components/ProfileCard";
 import { SearchForm } from "./components/SearchForm";
 import { PostCard } from "./components/PostCard";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
 
 export function Blog() {
+    const [posts, setPosts] = useState([])
+
+    async function fetchPosts(query?: string) {
+        const response = await api.get(
+            `search/issues?q=${query}is:issue%20is:open%20repo:vilmarsitiodigital/github-blog`
+        )
+        setPosts(response.data.items)
+        console.log(response.data.items)
+    }
+
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
     return (
         <BlogContainer>
             <ProfileCard />
