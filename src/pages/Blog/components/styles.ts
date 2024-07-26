6d0d044636955bf5601fe50c27f314db4eb7c4cc
tiagoforward <devtiagoforward@gmail.com>
2024-07-26 11:50:38 -0300

    finished input style

--- src/pages/Blog/components/styles.ts
+++ src/pages/Blog/components/styles.ts
@@ -28,7 +28,7 @@ export const SearchFormContainer = styled.form`
         border-radius: 6px;
         border: 1px solid ${props => props.theme["base-border"]};
         background-color: ${props => props.theme["base-input"]};
-        color: ${props => props.theme["base-label"]};
+        color: ${props => props.theme["base-text"]};
         padding: 1rem;
 
         &::placeholder {
