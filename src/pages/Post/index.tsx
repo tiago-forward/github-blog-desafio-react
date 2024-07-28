a38e6d13ecc6be26fcde1c3c67fffb62f039233d
tiagoforward <devtiagoforward@gmail.com>
2024-07-28 16:09:41 -0300

    finished section of the post

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,9 +1,93 @@
-import { PostContainer } from "./styles";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faBuilding, faChevronLeft, faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { Link } from "../../components/Link";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+
+import { LinkReturn } from "./styles";
+import { Container, PostContainer, PostContent, PostInfoContent } from "./styles";
+
+interface PostInfo {
+    title: string
+    comments: number
+    createdAt: string
+    githubUsername: string
+    url: string
+    body: string
+}
 
 export function Post() {
+    const [postInfo, setPostInfo] = useState<PostInfo | null>(null)
+
+    async function fetchPost() {
+        try {
+            const response = await api.get(`repos/rocketseat-education/reactjs-github-blog-challenge/issues/1 `)
+            const {
+                title,
+                comments,
+                created_at: createdAt,
+                user,
+                html_url: url,
+                body,
+            } = response.data
+            setPostInfo({
+                title,
+                comments,
+                createdAt,
+                githubUsername: user.login,
+                url: url,
+                body,
+            })
+            console.log(response.data)
+        } catch (error) {
+            console.error("Erro ao buscar dados do usuário:", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchPost()
+    }, [])
+
     return (
-        <PostContainer>
-            <h1>Seção Post</h1>
-        </PostContainer>
+        <Container>
+            <PostContainer>
+                <PostContent>
+                    <header>
+                        <LinkReturn href="/">
+                            <FontAwesomeIcon icon={faChevronLeft} />
+                            Voltar
+                        </LinkReturn>
+                        <Link href={postInfo?.url} target="_blank">Ver no github</Link>
+                    </header>
+
+                    <main>
+                        <h1 className="title"> {postInfo?.title !== null ? postInfo?.title : 'Nenhum título registrado'}</h1>
+                    </main>
+
+                    <footer>
+                        <ul className="info">
+                            <li className="username">
+                                <FontAwesomeIcon icon={faGithub} className="icon" />
+                                {postInfo?.githubUsername}
+                            </li>
+                            <li className="username">
+                                <FontAwesomeIcon icon={faBuilding} className="icon" />
+                                {postInfo?.createdAt !== null ? postInfo?.createdAt : 'Nenhuma empresa registrada'}
+                            </li>
+                            <li className="username">
+                                <FontAwesomeIcon icon={faUserGroup} className="icon" />
+                                {`${postInfo?.comments} comentários`}
+                            </li>
+                        </ul>
+                    </footer>
+                </PostContent>
+            </PostContainer>
+
+            <PostInfoContent>
+                <p>{postInfo?.body}</p>
+            </PostInfoContent>
+        </Container>
     )
 }
--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -1,8 +1,103 @@
 import styled from "styled-components";
 
-export const PostContainer = styled.main`
+export const Container = styled.main`
   width: 100%;
   max-width: 1120px;
   margin: 4rem auto 0;
   padding: 0 1.5rem;
+`
+
+export const PostContainer = styled.section`
+    width: 100%;
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 0 1.5rem;
+    border-radius: 10px;
+    margin-top: -9rem;
+
+    display: flex;
+    align-items: center;
+
+    background-color: ${props => props.theme['base-profile']};
+
+    .avatar {
+        border-radius: 10px;
+        width: 9.25rem;
+        height: 9.25rem;
+    }
+`
+
+export const PostContent = styled.div`
+    padding: 2rem 2.5rem;
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .name {
+            font-size: 1.5rem;
+        }
+    }
+
+    main, .title {
+        font-size: 1.5rem;
+        color: ${props => props.theme['base-title']};
+    }
+
+    footer, .info {
+        list-style: none;
+        
+        margin-top: 0.5rem;
+
+        display: flex;
+        align-items: center;
+        gap: 1.5rem;
+
+        .username {
+            font-size: 1rem;
+            color: ${props => props.theme['base-span']};
+            
+            display: flex;
+            align-items: center;
+            gap: 0.5rem;
+
+            .icon {
+                color: ${props => props.theme['base-label']};
+            }
+        }
+    }
+`
+
+export const PostInfoContent = styled.section`
+    padding: 2.5rem 2rem;
+    
+    display: flex;
+    flex-direction: column;
+    
+    color: ${props => props.theme["base-text"]};
+    font-size: 1rem;
+`
+
+export const LinkReturn = styled.a`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    text-transform: uppercase;
+    text-decoration: none;
+    font-size: 0.75rem;
+    font-weight: 700;
+
+    color: ${(props) => props.theme.blue};
+    border-bottom: 1px solid transparent;
+
+    &:hover {
+        border-bottom: 1px solid ${(props) => props.theme.blue};
+    }
 `
